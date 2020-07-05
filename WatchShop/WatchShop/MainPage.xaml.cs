79c48d98d736ae964e2faba9fdc2717829b1f331
Oludayo Alli <sunnymexy@gmail.com>
2020-07-05 06:35:24 +0100

    Add project files.

---
+++ WatchShop/WatchShop/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WatchShop
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMenu()
+        {
+            MenuGrid.IsVisible = true;
+
+            Action<double> callback = input => MenuView.TranslationX = input;
+            MenuView.Animate("anim", callback, -260, 0, 16, 300, Easing.CubicInOut);
+        }
+
+        private void CloseMenu()
+        {
+            Action<double> callback = input => MenuView.TranslationX = input;
+            MenuView.Animate("anim", callback, 0, -260, 16, 300, Easing.CubicInOut);
+
+            MenuGrid.IsVisible = false;
+        }
+
+
+        private void MenuTapped(object sender, EventArgs e)
+        {
+            OpenMenu();
+        }
+
+        private void OverlayTapped(object sender, EventArgs e)
+        {
+            CloseMenu();
+        }
+    }
+}
---
+++ WatchShop/WatchShop/ViewModel/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WatchShop.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            Products = GetProducts();
+            MenuList = GetMenus();
+        }
+
+        private ObservableCollection<Product> products;
+        public ObservableCollection<Product> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+
+        private ObservableCollection<Menu> menuList;
+        public ObservableCollection<Menu> MenuList
+        {
+            get { return menuList; }
+            set { menuList = value; }
+        }
+
+        private ObservableCollection<Menu> GetMenus()
+        {
+            return new ObservableCollection<Menu>
+            {
+                new Menu { Icon = "order.png", Name = "My Order"},
+                new Menu { Icon = "favorite.png", Name = "My WishList"},
+                new Menu { Icon = "shopping.png", Name = "Shopping Cart"},
+                new Menu { Icon = "settings.png", Name = "Settings"}
+            };
+        }
+
+        private ObservableCollection<Product> GetProducts()
+        {
+            return new ObservableCollection<Product>
+            {
+                new Product { Name = "Hilfi Watch", Price = 789.99f, Image = "charlesWatch.png", Model = "Model 2011", Rating = 4.5, Views = 4.5},
+                new Product { Name = "Gold Watch", Price = 697.99f, Image = "johnWatch.png", Model = "Model 2009", Rating = 4.5, Views = 4.5},
+                new Product { Name = "Pierre LD Watch", Price = 897.99f, Image = "marekWatch.png", Model = "Model 2070", Rating = 4.5, Views = 4.5},
+                new Product { Name = "Omega RD Watch", Price = 567.99f, Image = "rutgeWatch.png", Model = "Model 1997", Rating = 4.5, Views = 4.5},
+            };
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Image { get; set; }
+        public double Rating { get; set; }
+        public double Views { get; set; }
+        public float Price { get; set; }
+    }
+
+    public class Menu
+    {
+        public string Name { get; set; }
+        public string Icon { get; set; }
+    }
+}
